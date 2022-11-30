0e14ddd92f5b7e3ca245d847efae6fe2e5dc5f7b
Ísis <101017585+isisnakashima@users.noreply.github.com>
2022-11-30 16:37:23 -0300

    Update cobrinha.js

--- cobrinha.js
+++ cobrinha.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d"); // ele que faz o desenho do jogo e 2d é como ele vai aparecer
 let box = 32;
-let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
+let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos.
 snake[0] ={
     x: 8 * box,
     y: 8 * box
